a64258980aa6653f6c3b931825db44694a67453a
TheusVask <matheus_vasconcelos98@hotmail.com>
2020-06-29 22:31:26 -0300

    Link pagina superman

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-let hqSuperman = document.getElementById("hq-superman")
+let img = document.querySelectorAll('#novidades-hqs img');
 
-hqSuperman.onclick = function(){
-    window.open("./Space_Comics - Copia/index.html");
+for (let i = 0; i < img.length; i++) {
+    img[i].onmouseover = function () {
+        this.classList.remove('shrink');
+        this.classList.add('grow');
+    };
+    img[i].onmouseout = function () {
+        this.classList.remove('grow');
+        this.classList.add('shrink');
+    };
 }
+
+
+let hqsuperman = document.getElementById("hqSuperman");
+
+hqsuperman.onclick = function(){
+    window.open('./Space_Comics - Copia/index.html')
+}
+

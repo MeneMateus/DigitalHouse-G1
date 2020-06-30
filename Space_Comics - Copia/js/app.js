9427fe2bed91589e8a57f88c7df23734ae33221b
J0N0S <jvaraujocosta13@gmail.com>
2020-06-30 12:17:17 -0300

    animação descricao

--- Space_Comics - Copia/js/app.js
+++ Space_Comics - Copia/js/app.js
@@ -1,33 +1,32 @@
-<<<<<<< HEAD
-gsap.from('#super-man' , {
-    duration: 2,
-    opacity: 0,
-    scale: 0.5,
-})
 
-gsap.from('.descrip-box', {
-    duration: 4,
-    opacity: 0,
-    scale: 0.3,
-    delay: 1
-})
+    gsap.from('#super-man' , {
+        duration: 2,
+        opacity: 0,
+        scale: 0.5,
+    })
+    
+    gsap.from('.descrip-box', {
+        duration: 4,
+        opacity: 0,
+        scale: 0.3,
+        delay: 1
+    })
+    
+    gsap.from('.botao', {
+        duration: 4,
+        opacity: 0,
+        scale: 0.3,
+        delay: 2
+    })
+    
+    let fechar = document.getElementById('fechar');
+    
+    fechar.addEventListener("click", function(){
+        window.location.href = ('../home.html')
+    })
 
-gsap.from('.botao', {
-    duration: 4,
-    opacity: 0,
-    scale: 0.3,
-    delay: 2
-})
-
-let fechar = document.getElementById('fechar');
-
-fechar.addEventListener("click", function(){
-    window.location.replace('file:///C:/Users/Jo%C3%A3o%20Victor/Desktop/digitalhouse/home.html')
-})
-=======
 let logo = document.getElementById('logo-header')
 
 logo.onclick = function(){
     window.location.href = '../home.html'
 }
->>>>>>> e43f5036a803c8f33f3608dd7d52c17bffd7aa95
